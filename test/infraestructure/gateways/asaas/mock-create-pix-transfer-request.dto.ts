d275cd66a6c5983c7a3e53307df8c02c0685e164
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-05 00:29:17 -0300

    feat: update mockCreatePixTransferRequestDto with new CPF and add transactionId

--- test/infraestructure/gateways/asaas/mock-create-pix-transfer-request.dto.ts
+++ test/infraestructure/gateways/asaas/mock-create-pix-transfer-request.dto.ts
@@ -1,12 +1,14 @@
+import { randomUUID } from "crypto";
 import { CreatePixTransferRequestDto } from "src/infrastructure/gateways/asaas/dto/create-pix-transfer-request.dto";
 import { PixAddressKeyType } from "src/infrastructure/gateways/asaas/dto/pix-address-key-type";
 
 export const mockCreatePixTransferRequestDto = (): CreatePixTransferRequestDto => {
   return new CreatePixTransferRequestDto(
     10.0,
-    '09493012301',
+    '99991111140',
     PixAddressKeyType.CPF,
     'Churrasco pago via Pix com chave',
-    null
+    null,
+    randomUUID().toString()
   );
 };
