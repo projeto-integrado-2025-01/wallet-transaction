d82dd597deb53c7d8f161303595d3581044f879d
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-05 00:32:46 -0300

    feat: update createTransfer method to return CreatePixTransferResponseDto

--- src/infrastructure/gateways/asaas/asaas-transfer-client.ts
+++ src/infrastructure/gateways/asaas/asaas-transfer-client.ts
@@ -1,5 +1,6 @@
 import { HttpPostClient } from "src/infrastructure/http/http-post-client";
 import { CreatePixTransferRequestDto } from "./dto/create-pix-transfer-request.dto";
+import { CreatePixTransferResponseDto } from "./dto/create-pix-transfer-response.dto";
 
 export class AsaasTransferClient {
   private readonly baseUrl = 'https://api-sandbox.asaas.com/v3';
@@ -9,14 +10,14 @@ export class AsaasTransferClient {
     private readonly apiKey: string
   ) {}
 
-  async createTransfer(params: CreatePixTransferRequestDto): Promise<any> {
+  async createTransfer(params: CreatePixTransferRequestDto): Promise<CreatePixTransferResponseDto> {
     const url = `${this.baseUrl}/transfers`;
 
     const headers = {
       access_token: this.apiKey,
     };
 
-    const response = await this.httpPostClient.post<any>({
+    const response = await this.httpPostClient.post<CreatePixTransferResponseDto>({
       url,
       body: params,
       headers,
