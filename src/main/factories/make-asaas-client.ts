59b9c2d03a1d5fb8ff637849d1674c1fb114fbb2
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-05 00:33:13 -0300

    feat: add factory function for AsaasTransferClient initialization

---
+++ src/main/factories/make-asaas-client.ts
@@ -0,0 +1,12 @@
+// main/factories/infra/asaas-transfer-client.factory.ts
+
+import { AsaasTransferClient } from "src/infrastructure/gateways/asaas/asaas-transfer-client";
+import { AxiosHttpClient } from "src/infrastructure/http/axios-http-client";
+
+
+export const makeAsaasTransferClient = (): AsaasTransferClient => {
+  const httpClient = new AxiosHttpClient();
+  const apiKey = process.env.ASAAS_API_KEY || '';
+
+  return new AsaasTransferClient(httpClient, apiKey);
+};
