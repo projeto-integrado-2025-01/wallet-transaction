2112567059a2a9e35079e96f4740f8532c908643
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-05 20:36:09 -0300

    feat: adicionar processamento de transações PIX com cliente Asaas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { makeQueueConsumer } from './main/factories/make-rabbitmq-consumer';
+import { makeAsaasTransferClient } from './main/factories/make-asaas-client';
+import { mockCreatePixTransferRequestDto } from 'test/infraestructure/gateways/asaas/mock-create-pix-transfer-request.dto';
+import { CreatePixTransferRequestDto } from './infrastructure/gateways/asaas/dto/create-pix-transfer-request.dto';
+import { randomUUID } from 'crypto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +13,31 @@ async function bootstrap() {
   const queueConsumer = makeQueueConsumer();
   await queueConsumer.consume({
     queue: 'finance.transaction.process',
-    onMessage: async (message) => {
-      console.log('Mensagem recebida:', message);
+    onMessage: async (message: any) => {
+      const content = message.content.toString();
+      const parsed = JSON.parse(content);
+
+      const { pattern, data } = parsed;
+
+      if (pattern === 'SINGLE_TRANSACTION_CREATED') {
+        const { value, pixKey, pixKeyType } = data;
+
+        console.log('Processando transação PIX:');
+
+        const asaasTransferClient = makeAsaasTransferClient();
+
+        const dto = new CreatePixTransferRequestDto(
+          value,
+          pixKey,
+          pixKeyType,
+          null,
+          null,
+          randomUUID().toString()
+        );
+
+        const transfer = await asaasTransferClient.createTransfer(dto)
+
+      }
     }
   });
 }
