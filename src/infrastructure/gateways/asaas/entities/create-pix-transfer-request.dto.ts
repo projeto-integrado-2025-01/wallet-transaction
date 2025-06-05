c78193dd9ea21404d1757500e55c0d7b1a8d1cef
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-04 22:47:13 -0300

    feat: add CreatePixTransferRequestDto for Pix transfer requests

---
+++ src/infrastructure/gateways/asaas/entities/create-pix-transfer-request.dto.ts
@@ -0,0 +1,11 @@
+import { PixAddressKeyType } from '../entities/pix-address-key-type';
+
+export class CreatePixTransferRequestDto {
+    constructor(
+      public readonly value: number,
+      public readonly pixAddressKey: string,
+      public readonly pixAddressKeyType: PixAddressKeyType,
+      public readonly description?: string,
+      public readonly scheduleDate?: string | null,
+    ) {}
+  }
