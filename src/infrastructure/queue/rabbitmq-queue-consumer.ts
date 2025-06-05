b49ce1dffb844401318f21f88151e0b16ed36359
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-05 20:24:43 -0300

    feat: implement RabbitMqConsumer for handling message consumption from RabbitMQ

---
+++ src/infrastructure/queue/rabbitmq-queue-consumer.ts
@@ -0,0 +1,27 @@
+import * as amqp from 'amqplib';
+
+import { QueueConsumer } from './queue-consumer';
+import { QueueConsumeRequest } from './dto/queue-consumer-request.dto';
+
+export class RabbitMqConsumer implements QueueConsumer {
+    constructor(private readonly connectionUrl: string) {}
+  
+    async consume(params: QueueConsumeRequest): Promise<void> {
+      const connection = await amqp.connect(this.connectionUrl);
+      const channel = await connection.createChannel();
+  
+      await channel.assertQueue(params.queue, { durable: true });
+  
+      channel.consume(
+        params.queue,
+        async (msg) => {
+          if (msg) {
+            const content = JSON.parse(msg.content.toString());
+            await params.onMessage(content);
+            channel.ack(msg);
+          }
+        },
+        { noAck: false }
+      );
+    }
+  }
