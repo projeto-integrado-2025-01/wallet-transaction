8349d702b1ba3e630ec8c6202ca4da207133dd40
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-05 20:24:37 -0300

    feat: integrate RabbitMQ consumer for processing finance transactions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { makeQueueConsumer } from './main/factories/make-rabbitmq-consumer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const queueConsumer = makeQueueConsumer();
+  await queueConsumer.consume({
+    queue: 'finance.transaction.process',
+    onMessage: async (message) => {
+      console.log('Mensagem recebida:', message);
+    }
+  });
 }
+
 bootstrap();
