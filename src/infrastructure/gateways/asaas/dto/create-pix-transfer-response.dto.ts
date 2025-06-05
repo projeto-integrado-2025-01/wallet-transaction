b9e823878938f862cd31a2e8423f7321921e36c4
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-06-05 00:25:33 -0300

    feat: add CreatePixTransferResponseDto for handling Pix transfer responses

---
+++ src/infrastructure/gateways/asaas/dto/create-pix-transfer-response.dto.ts
@@ -0,0 +1,28 @@
+import { BankAccountDto } from "./bank-account.dto";
+
+export class CreatePixTransferResponseDto {
+    constructor(
+      public readonly id: string,
+      public readonly value: number,
+      public readonly netValue: number,
+      public readonly description: string,
+      public readonly type: string,
+      public readonly operationType: string,
+      public readonly status: string,
+      public readonly bankAccount: BankAccountDto,
+      public readonly walletId: string | null,
+      public readonly recurring: string | null,
+      public readonly transactionReceiptUrl: string | null,
+      public readonly endToEndIdentifier: string | null,
+      public readonly externalReference: string | null,
+      public readonly dateCreated: string,
+      public readonly canBeCancelled: boolean,
+      public readonly authorized: boolean,
+      public readonly transferFee: number,
+      public readonly scheduleDate: string | null,
+      public readonly failReason: string | null,
+      public readonly effectiveDate: string | null,
+      public readonly confirmedDate: string | null,
+      public readonly object: string
+    ) {}
+  }
